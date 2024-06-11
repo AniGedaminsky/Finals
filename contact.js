e619c23d0825725072497531f613c1fb953831bf
AniGedaminsky <gedaminskyani@gmail.com>
2024-06-11 13:31:32 +0400

    new push

---
+++ contact.js
@@ -0,0 +1,27 @@
+const validateEmail = (email) => {
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+const validate = () => {
+    const result = document.getElementById('result');
+    const email = document.getElementById('email').value;
+    result.textContent = '';
+
+    if (validateEmail(email)) {
+        result.textContent = email + ' is valid.';
+        result.style.color = 'green';
+    } else {
+        result.textContent = email + ' is invalid.';
+        result.style.color = 'red';
+    }
+};
+
+document.getElementById('email').addEventListener('input', validate);
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    // Add validation or other logic here if needed
+    window.location.href = 'index.html'; // Redirect to Main page
+});
